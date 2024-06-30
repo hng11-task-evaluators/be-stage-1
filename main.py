e70b21f087b3c7c245c4b1f301655b4952fa205a
zxenonx <xenon.nemrod@gmail.com>
2024-06-30 17:45:06 +0100

    Refactor response

--- main.py
+++ main.py
@@ -1,49 +1,11 @@
 from fastapi import FastAPI
 import httpx
 
-from checks import ERRORS, assign_score, validate_visitor
-from schema import VisitorOut
-from pydantic import ValidationError
-
-from fastapi.responses import JSONResponse
+from checks import is_valid_ip
 
 app = FastAPI()
 
-@app.exception_handler(ValidationError)
-async def validation_error_handler(request, exc):
-    print("exc", exc)
-    good_objects = []
-    bad_objects = []
-    errors = {}
-    
-    # Iterate over the list of objects
-    for index, obj in enumerate(exc.errors()):
-        try:
-            # Validate the object using pydantic
-            validated_obj = VisitorOut(**obj)
-            good_objects.append(validated_obj)
-        except ValidationError as e:
-            # If validation error, add the object to bad objects
-            # and the error message to errors dictionary
-            bad_objects.append(obj)
-            errors[index] = e.errors()
-    
-    # Upload all good objects to the database
-    #upload_to_database(good_objects)
-    
-    # Return the response with the list of good and bad objects
-    # and their errors, if any
-    print("custommerror", errors, good_objects, bad_objects)
-    return JSONResponse(
-        status_code=exc.code,
-        content={"message": f"Exception Occurred! Reason -> {exc.message}"},
-    )
-    #return {"good_objects": good_objects, "bad_objects": bad_objects, "errors": errors}
-
-
-timeout = httpx.Timeout(60, connect=30, read=30, write=10)
-
-#https://test-be.free.beeceptor.com/api/hello?visitor_name=Tom
+errors = []
 @app.get("/")
 async def root():
     return {"client": "Hello World"}   
@@ -51,41 +13,61 @@ async def root():
 # the test API
 @app.get("/api/hello")
 async def test(visitor_name: str = "Tom"):
-    return {"client_ip": "34.67.45.67", "location": "GHk", "greeting": f"No where {visitor_name}"}
+    return {"client_ip": "", "location": "", "greeting": f"Hello {visitor_name}"}
 
-async def call_api(api_url: str):
-    async with httpx.AsyncClient(timeout=timeout) as client:
-        response = await client.get(api_url)
-        print("async", response)
-        print("resp json", response.json())
+@app.post("/grade")
+async def grade(api_url: str):
+    global errors
+    errors = []  # Reset errors for each grading request
+    score = 10
 
-        visitor_name = response.request.url.params.get("visitor_name")
-        print(visitor_name)
+    try:
+        # Check if the API URL is reachable and visitor_name is included in the query params
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"{api_url}")
 
         if response.status_code != 200:
-            print("error validation", response.json())
+            errors.append(f"API did not return 200 OK, but {response.status_code}")
+            return {"score": 0, "errors": f"API did not return 200 OK, but {response.status_code}"}
+            #raise HTTPException(status_code=400, detail=f"API did not return 200 OK, but {response.status_code}")
+        
+        visitor_name = response.request.url.params.get("visitor_name")
+        # Check if the visitor_name query parameter is present in the request URL
+        if not visitor_name:
+            errors.append("Query parameter 'visitor_name' is missing")
+            score -= 2
+            #raise HTTPException(status_code=400, detail="Query parameter 'visitor_name' is missing")
+
+        response_data = response.json()
+        print("response_data", response_data)
 
 
-        print(response.json())    
-        return response.json() | {"visitor_name": visitor_name}
+        # Check if visitor's name is part of the greeting
+        if visitor_name and (visitor_name not in response_data.get("greeting")) or (not visitor_name and not response_data.get("greeting")):
+            errors.append("Visitor_name is missing in greeting")
+            score -= 2
 
+        # Check if JSON response contains the required properties
+        if not response_data.get("client_ip") or not is_valid_ip(response_data.get("client_ip")):
+            errors.append("Client ip is missing or invalid")
+            score -= 2
 
-@app.get("/grade-task-one")
-async def grade(api_url: str):
-    print(api_url)
-    response = await call_api(api_url)
-    print(response)
+        if not response_data.get("location"):
+            errors.append("Location is missing")
+            score -= 2
 
-    errors = validate_visitor(response) 
-    print("errors", errors)
+        if not response_data.get("greeting"):
+            errors.append("Greeting is missing")
+            score -= 2
 
-    if errors:
-        print("errors", ERRORS)
+        return {"score": score, "response": response_data, "errors": errors}
 
+    except Exception as e:
+        print("error", e)
+        errors.append(str(e))
+        return {"score": score, "response": response_data, "errors": errors}
+        #raise HTTPException(status_code=400, detail=str(e))
 
-    data = VisitorOut(**response)
-
-    score = assign_score(data.model_dump())
-    print("score", score)
-
-    return {"message": "task one passed"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
